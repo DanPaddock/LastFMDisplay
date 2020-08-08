df80a00717a94f66afb39d2435ed74e4c4872c88
DanPaddock <danpaddk@gmail.com>
2020-08-08 14:21:19 -0400

    transfer

--- fm-display/src/index.js
+++ fm-display/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { FMData } from './Components/FMData';
+import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+  <FMData
+    userName={'your-username'}
+    apiKey={'your-api-key'}
+  />
   </React.StrictMode>,
   document.getElementById('root')
 );
